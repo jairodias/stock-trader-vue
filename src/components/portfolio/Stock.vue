c4a4f8759ac837d2a811a09502bba824b49bcacd
jairodias <jairopereira_dias@hotmail.com>
2021-02-20 08:10:53 -0300

    [master] - Criando componentes com base no curso de vue js

---
+++ src/components/portfolio/Stock.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Ação (Portifolio)
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.id" :stock="stock" />
+    </v-layout>
+</template>
+
+<script>
+
+import Stock from './Stock'
+
+export default {
+    components: {
+        Stock
+    },
+    data() {
+        return {
+            stocks: [
+                {id: 1, name: 'BMW', price: 110},
+                {id: 2, name: 'Twitter', price: 200},
+                {id: 3, name: 'Google', price: 300},
+                {id: 4, name: 'Facebook', price: 280},
+                {id: 5, name: 'Instagram', price: 110},
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
