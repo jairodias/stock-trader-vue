fb65067cfb93e9aa4bdbc0b9f3cec6f29d580818
jairodias <jairopereira_dias@hotmail.com>
2021-02-20 08:26:13 -0300

    [master] - Criando os dados externalizados

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -12,15 +12,9 @@ export default {
     components: {
         Stock
     },
-    data() {
-        return {
-            stocks: [
-                {id: 1, name: 'BMW', price: 110},
-                {id: 2, name: 'Twitter', price: 200},
-                {id: 3, name: 'Google', price: 300},
-                {id: 4, name: 'Facebook', price: 280},
-                {id: 5, name: 'Instagram', price: 110},
-            ]
+    computed: {
+        stocks() {
+            return this.$store.getters.stocks
         }
     }
 }
---
+++ src/data/stocks.js
@@ -0,0 +1,7 @@
+export default [
+    { id: 1, name: 'BMW', price: 110 },
+    { id: 2, name: 'Twitter', price: 200 },
+    { id: 3, name: 'Google', price: 300 },
+    { id: 4, name: 'Facebook', price: 280 },
+    { id: 5, name: 'Instagram', price: 110 }
+]
